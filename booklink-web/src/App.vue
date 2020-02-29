efbeb5827064568c07f1383b58729b7b0bffaee9
Adam Zimowski <mrazjava@yandex.com>
2020-02-29 17:19:16 +0100

    proof of concept counter
    Calls backend REST.

---
+++ booklink-web/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Booklink</h1>
+    <hr/>
+    <book-info :count="countik" />
+    <hr/>
+    <div style="text-align:right;">Proof of concept.</div>
+  </div>
+</template>
+
+<script>
+  import BookInfo from '@/components/BookInfo.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      BookInfo,
+    },
+    data() {
+      return {
+        countik: 0
+      }
+    },
+    mounted() {
+      this.getCountTest()
+    },
+    methods: {
+      async getCountTest() {
+        try {
+          const response = await fetch('http://localhost:8080/rest/v1/book/count-all')
+          const data = await response.json()
+          this.countik = data
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+  .small-container {
+    max-width: 680px;
+    text-align: center;
+  }
+</style>
---
+++ booklink-web/src/components/BookInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="book-info">
+    <div class="count" v-bind:class="{'count-p': isPositive(), 'count-n': !isPositive()}">
+      {{ count }}
+    </div>
+    <div class="footnote">
+      Refresh page to get a new count. Count is fetched from booklink-backend over rest
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'book-info',
+    props: {
+      count: Number
+    },
+    methods: {
+    isPositive() {
+      return this.count>0;
+    }
+    }
+  }
+</script>
+
+<style scoped>
+  #book-info {
+
+  }
+  button {
+    margin: 0 0.5rem 0 0;
+  }
+  .footnote {
+    font-style: italic;
+    font-size: 0.8em;
+  }
+  .count {
+    font-size: 2.5em;
+  }
+  .count-p {
+    color: green;
+  }
+  .count-n {
+    color: red;
+  }
+</style>
