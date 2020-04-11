00f731b1bc1ea9ad5896c6e317cdb1888eb42ee7
Adam Zimowski <mrazjava@yandex.com>
2020-04-11 10:26:30 +0200

    externalized technical info into a component

---
+++ src/components/TechnicalInfo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="technical-info">
+    <table>
+      <col class="techInfo-col1" />
+      <col class="techInfo-col2" />
+      <tr>
+        <td>Environment:</td>
+        <td>{{techFeEnv}}</td>
+      </tr>
+      <tr>
+        <td>FE version:</td>
+        <td>{{techFeVersion}}</td>
+      </tr>
+      <tr>
+        <td>FE branch:</td>
+        <td>{{techFeBranch}}</td>
+      </tr>
+      <tr>
+        <td>FE last commit:</td>
+        <td>{{techFeCommit}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import Configuration from '@/util/configuration'
+
+  export default {
+    name: 'TechnicalInfo',
+    data() {
+      return {
+        backendHost: Configuration.value('BACKEND_HOST'),
+        techFeEnv: Configuration.value('RUNNING_ENV'),
+        techFeVersion: process.env.VUE_APP_VERSION,
+        techFeCommit: process.env.VUE_APP_GIT_SHORT_COMMIT_HASH,
+        techFeBranch: process.env.VUE_APP_GIT_BRANCH,
+      };
+    },
+    mounted() {
+    },
+    props: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+  table, th, td {
+    border: solid 0px black;
+  }
+  table {
+    margin-left: 20px;
+    font-family: "Courier New", Courer;
+  }
+  td {
+    padding: 0;
+  }
+  .techInfo-col1 {
+    width: 220px;
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -10,65 +10,24 @@
       </p>
 
       <h2 id="headerTech">Technical Info</h2>
-        <table>
-          <col class="techInfo-col1" />
-          <col class="techInfo-col2" />
-          <tr>
-            <td>Environment:</td>
-            <td>{{techFeEnv}}</td>
-          </tr>
-          <tr>
-            <td>FE version:</td>
-            <td>{{techFeVersion}}</td>
-          </tr>
-          <tr>
-            <td>FE branch:</td>
-            <td>{{techFeBranch}}</td>
-          </tr>
-          <tr>
-            <td>FE last commit:</td>
-            <td>{{techFeCommit}}</td>
-          </tr>
-        </table>
+      <technical-info />
+
     </div>
   </layout>
 </template>
 
 <script>
 import Layout from '@/layouts/Layout';
-import Configuration from '@/util/configuration'
+import TechnicalInfo from '@/components/TechnicalInfo'
 
 export default {
   name: `About`,
   components: {
     Layout,
-  },
-  data() {
-    return {
-      techFeEnv: Configuration.value('RUNNING_ENV'),
-      techFeVersion: process.env.VUE_APP_VERSION,
-      techFeCommit: process.env.VUE_APP_GIT_SHORT_COMMIT_HASH,
-      techFeBranch: process.env.VUE_APP_GIT_BRANCH,
-    };
-  },
+    TechnicalInfo
+  }
 };
 </script>
 
 <style lang="scss">
-  table, th, td {
-    border: solid 0px black;
-  }
-  table {
-    margin-left: 20px;
-    font-family: "Courier New", Courer;
-  }
-  td {
-    padding: 0;
-  }
-  .techInfo-col1 {
-    width: 220px;
-  }
-  #headerTech {
-    margin-bottom:10px;
-  }
 </style>
