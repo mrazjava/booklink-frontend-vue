3e98db479674076678edce231d99b7c5816d8160
Adam Zimowski <mrazjava@yandex.com>
2020-04-11 12:09:36 +0200

    display backend technical info

--- src/components/TechnicalInfo.vue
+++ src/components/TechnicalInfo.vue
@@ -4,7 +4,7 @@
       <col class="techInfo-col1" />
       <col class="techInfo-col2" />
       <tr>
-        <td>Environment:</td>
+        <td>FE Environment:</td>
         <td>{{techFeEnv}}</td>
       </tr>
       <tr>
@@ -19,6 +19,22 @@
         <td>FE last commit:</td>
         <td>{{techFeCommit}}</td>
       </tr>
+      <tr>
+        <td>BE Environment:</td>
+        <td>{{techBeEnv}}</td>
+      </tr>
+      <tr>
+        <td>BE version:</td>
+        <td>{{techBeVersion}}</td>
+      </tr>
+      <tr>
+        <td>BE branch:</td>
+        <td>{{techBeBranch}}</td>
+      </tr>
+      <tr>
+        <td>BE last commit:</td>
+        <td>{{techBeLastCommit}}</td>
+      </tr>
     </table>
   </div>
 </template>
@@ -29,35 +45,54 @@ import Configuration from '@/util/configuration'
 export default {
   name: 'TechnicalInfo',
   data() {
+    const FETCH = "checking ..."
     return {
       backendHost: Configuration.value('BACKEND_HOST'),
       techFeEnv: Configuration.value('RUNNING_ENV'),
       techFeVersion: process.env.VUE_APP_VERSION,
       techFeCommit: process.env.VUE_APP_GIT_SHORT_COMMIT_HASH,
       techFeBranch: process.env.VUE_APP_GIT_BRANCH,
+      techBeEnv: FETCH,
+      techBeVersion: FETCH,
+      techBeBranch: FETCH,
+      techBeLastCommit: FETCH
     };
   },
   mounted() {
+    this.getBackendInfo();
   },
   props: {
   },
   methods: {
+    async getBackendInfo() {
+      const response = await this.$http.get(
+        this.backendHost + '/actuator/info', {
+          headers: {
+          }
+        });
+        //console.log(response);
+        const result = response.data;
+        this.techBeEnv = result.build.environment;
+        this.techBeVersion = result.build.maven.version;
+        this.techBeBranch = result.git.branch;
+        this.techBeLastCommit = result.git.commit.id;
+      }
+    }
   }
-}
-</script>
+  </script>
 
-<style scoped>
-table, th, td {
-  border: solid 0px black;
-}
-table {
-  margin-left: 20px;
-  font-family: "Courier New", Courer;
-}
-td {
-  padding: 0;
-}
-.techInfo-col1 {
-  width: 220px;
-}
-</style>
+  <style scoped>
+  table, th, td {
+    border: solid 0px black;
+  }
+  table {
+    margin-left: 20px;
+    font-family: "Courier New", Courer;
+  }
+  td {
+    padding: 0;
+  }
+  .techInfo-col1 {
+    width: 220px;
+  }
+  </style>
