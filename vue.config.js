43de3eaf4444ba52e84724ef1051700ad4c28179
Adam Zimowski <mrazjava@yandex.com>
2020-05-08 13:59:09 +0200

    configure branch for git revision plugin

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,9 @@ module.exports = {
   lintOnSave: true,
   'chainWebpack': config => {
     config.plugin('define').tap(args => {
-      const gitRevisionPlugin = new GitRevisionPlugin()
+      const gitRevisionPlugin = new GitRevisionPlugin({
+        branch: true
+      })
       args[0]['process.env']['VUE_APP_VERSION'] = JSON.stringify(require('./package.json').version)
       args[0]['process.env']['VUE_APP_GIT_SHORT_COMMIT_HASH'] = JSON.stringify(gitRevisionPlugin.version())
       args[0]['process.env']['VUE_APP_GIT_LONG_COMMIT_HASH'] = JSON.stringify(gitRevisionPlugin.commithash())
