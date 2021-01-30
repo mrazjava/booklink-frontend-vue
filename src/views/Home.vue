c369632759221353bbbf3883ca099c815ec08fbf
Adam Zimowski <mrazjava@protonmail.com>
2021-01-30 10:48:09 +0100

    search works ui proto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,13 @@
     <random-work :fWork="featuredWork" :fAuthor="featuredAuthor" @depot-work-updated="updateDepotWork" />
     <div class="Home__intro">
         Blog on books with browsable cross reference.
+        <p/>
+        <autocomplete
+          :search="searchWorks"
+          placeholder="Search Works"
+          aria-label="Search Works"
+          :get-result-value="getResultValue"
+          @submit="onSubmit"></autocomplete>
     </div>
   </div>
 </template>
@@ -25,12 +32,45 @@ export default {
     updateDepotWork(work, author) {
       this.featuredWork = work;
       this.featuredAuthor = author;
+    },
+    searchWorks(input) {
+
+          return new Promise((resolve) => {
+            if (input.length < 3) {
+              return resolve([])
+            }
+
+            resolve(["abc", "def", "ghi"])
+
+            //fetch(url)
+            //  .then((response) => response.json())
+            //  .then((data) => {
+            //    resolve(data.query.search)
+        //})
+      })
+    },
+    getResultValue(result) {
+      return "abrakadabra";
+    },
+    onSubmit(result) {
+      alert('boo');
     }
   }
 };
 </script>
 
+<style type="text/css">
+.autocomplete-input {
+  width: 50%;
+}
+.autocomplete-result-list {
+  width: 50% !important;
+}
+</style>
 <style lang="scss" scoped>
+  .sandbox {
+    font-size:0.8em;
+  }
   .Home {
     &__intro {
       margin-top: 40px;
